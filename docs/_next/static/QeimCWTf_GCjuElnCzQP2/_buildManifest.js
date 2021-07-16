5b8a547ba206661930df43a9ddc7600979679fd8
tryout <justadummymail123@gmail.com>
2021-07-17 02:49:24 +0530

    doc modify

---
+++ docs/_next/static/QeimCWTf_GCjuElnCzQP2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-45b4d8bd8e94a5a4c832.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   assetPrefix: '.',
   images: {
-    loader: 'imgix'
+    loader: 'imgix',
+    path: ''
   },
 }
